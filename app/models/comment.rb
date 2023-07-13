4088463fa725392173faf4d70cffc5488273d7f9
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-07-13 10:56:55 +0200

    fix linter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    def update_comments_counter
-        post.update_comments_counter
-    end
+  belongs_to :user
+  belongs_to :post
+  def update_comments_counter
+    post.update_comments_counter
+  end
 end
