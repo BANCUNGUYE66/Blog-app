d37f75399930175728fc57ea8cbba8220b483c9f
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-07-27 02:50:46 +0300

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
-class PostsController < ApplicationController
-  def index; end
+class PostsController < ActionController::Base
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user # Set the author for the new post (assuming you have a current_user method)
+    @post.save
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :author_id)
+  end
 end
