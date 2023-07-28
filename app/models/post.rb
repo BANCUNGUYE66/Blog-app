71642b5bbe78fec23d6c1401a9fd4f92e9c01769
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-07-28 17:10:00 +0300

    add changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,28 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes
+  belongs_to :author, class_name: 'User'
   has_many :comments
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  has_many :likes
 
   after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  after_initialize :set_defaults
+
+  def return_five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
 
   def update_posts_counter
     author.increment!(:posts_counter)
   end
 
-  def recent_comments
-    comments.order('created_at Desc').limit(5)
+  def set_defaults
+    self.likes_counter ||= 0
+    self.comments_counter ||= 0
   end
 end
